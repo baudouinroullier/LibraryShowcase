d334375c6e48918c43453052720d161e8c3b0990
Baudouin Roullier <baudouin.roullier@exotec.com>
2023-04-11 20:31:12 +0200

    add fmt to project

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,9 @@
 #include "../src/Matrix.h"
-#include <cstdio>
+#include <fmt/core.h>
 
 int main()
 {
     bla::Vector2 test{1,1};
-    printf("Hello! %f %f\n", test(0), test(1));
+    fmt::print("Hello! {} {}\n", test(0), test(1));
     return 0;
 }
