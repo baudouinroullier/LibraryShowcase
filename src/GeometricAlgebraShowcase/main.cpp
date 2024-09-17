2f63b728154059998ceb76f94141ea96aa8d8824
Baudouin Roullier <baudouin.roullier@exotec.com>
2024-09-17 14:37:22 +0200

    have fun with multivector display

--- src/GeometricAlgebraShowcase/main.cpp
+++ src/GeometricAlgebraShowcase/main.cpp
@@ -19,7 +19,7 @@ public:
         s->setOrigin(width, width);
         s->setFillColor(sf::Color::Transparent);
         s->setOutlineColor(sf::Color::Black);
-        s->setOutlineThickness(2);
+        s->setOutlineThickness(4);
         s->setPosition(500, 500);
 
         sf::CircleShape* e = m_endPoint.makeShape<sf::CircleShape>(width);
@@ -27,16 +27,20 @@ public:
         e->setFillColor(sf::Color::Transparent);
         e->setOutlineColor(sf::Color::Black);
         e->setOutlineThickness(2);
-        e->setPosition(600, 500);
+        e->setPosition(750, 500);
+
+        m_startPos = {500, 500};
+        m_endPos = {750, 500};
 
         m_startPoint.makeInteraction<act::Drag>([this](sf::Shape& shape, bool active)
                                                 {
-                                                    shape.setOutlineThickness(shape.getOutlineThickness() + active ? 2 : -2);
-                                                    setStartPosition(m_startPoint.getShape()->getPosition());
+                                                    shape.setOutlineThickness(active ? 4 : 2);
+                                                    setPosition(m_startPoint.getShape()->getPosition());
+                                                    m_endPoint.getShape()->setPosition(m_endPos);
                                                 });
         m_endPoint.makeInteraction<act::Drag>([this](sf::Shape& shape, bool active)
                                               {
-                                                  shape.setOutlineThickness(shape.getOutlineThickness() + active ? 2 : -2);
+                                                  shape.setOutlineThickness(active ? 4 : 2);
                                                   setEndPosition(m_endPoint.getShape()->getPosition());
                                               });
     }
@@ -70,6 +74,25 @@ int main()
     MovableArrow b{10, sf::Color::Blue};
     act::ArrowShape c{10, sf::Color::Magenta};
 
+    sf::RectangleShape axes[6];
+    axes[0].setSize({1000, 2});
+    axes[1].setSize({2, 1000});
+    axes[2].setSize({50, 2});
+    axes[3].setSize({50, 2});
+    axes[4].setSize({2, 50});
+    axes[5].setSize({2, 50});
+    for (sf::RectangleShape& a : axes)
+    {
+        a.setOrigin(a.getSize() / 2.f);
+        a.setFillColor({128,128,128});
+    }
+    axes[0].setPosition({2*scale, 2*scale});
+    axes[1].setPosition({2*scale, 2*scale});
+    axes[2].setPosition({2*scale, 2*scale+scale});
+    axes[3].setPosition({2*scale, 2*scale-scale});
+    axes[4].setPosition({2*scale+scale, 2*scale});
+    axes[5].setPosition({2*scale-scale, 2*scale});
+
     while (window.isOpen())
     {
         while (window.pollEvent(event))
@@ -82,15 +105,27 @@ int main()
             b.m_startPoint.processEvent(event) ||
             b.m_endPoint.processEvent(event);
 
-            galg::Multivector2 mvA = galg::Multivector2{0, a.m_startPoint.getShape()->getPosition().x - 2*scale, a.m_startPoint.getShape()->getPosition().y - 2*scale, 0} / scale;
-            galg::Multivector2 mvB = galg::Multivector2{0, b.m_startPoint.getShape()->getPosition().x - 2*scale, b.m_startPoint.getShape()->getPosition().y - 2*scale, 0} / scale;
-            galg::Multivector2 mvC = scale*(mvA + mvB);
+            sf::Vector2f aStart = a.m_startPoint.getShape()->getPosition();
+            sf::Vector2f aEnd = a.m_endPoint.getShape()->getPosition();
+            sf::Vector2f bStart = b.m_startPoint.getShape()->getPosition();
+            sf::Vector2f bEnd = b.m_endPoint.getShape()->getPosition();
+            galg::Multivector2 mvA = galg::Multivector2{(aEnd - aStart).x, aStart.x - 2 * scale, aStart.y - 2 * scale, (aEnd - aStart).y} / scale;
+            galg::Multivector2 mvB = galg::Multivector2{(bEnd - bStart).x, bStart.x - 2 * scale, bStart.y - 2 * scale, (bEnd - bStart).y} / scale;
+            galg::Multivector2 mvC = (mvA * mvB);
 
-            c.setStartPosition(mvC.vector().x() + 2*scale, mvC.vector().y() + 2*scale);
-            c.setEndPosition(2*scale, 2*scale);
+            fmt::print("{:.3f} * {:.3f} = {:.3f}\n", mvA.vector(), mvB.vector(), mvC.vector());
+            fflush(stdout);
+
+            mvC *= scale;
+            c.setStartPosition(0, 0);
+            c.setEndPosition(mvC.rotor().real(), mvC.rotor().imag());
+            c.move(mvC.vector().x() + 2*scale, mvC.vector().y() + 2*scale);
         }
 
         window.clear(sf::Color::White);
+
+        for (sf::RectangleShape& a : axes)
+            window.draw(a);
 
         window.draw(a);
         window.draw(b);
