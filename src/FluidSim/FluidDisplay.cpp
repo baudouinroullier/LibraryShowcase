58075e8957f9487b4d44593daac60e9887c776e2
Baudouin Roullier <baudouin.roullier@exotec.com>
2025-11-07 15:21:02 +0100

    slightly offset display

--- src/FluidSim/FluidDisplay.cpp
+++ src/FluidSim/FluidDisplay.cpp
@@ -4,17 +4,18 @@
 FluidDisplay::FluidDisplay(const FluidSim& sim) :
     m_sim(sim)
 {
-    for (int i = 0; i < N - 1; ++i)
+    for (int i = 0; i < N; ++i)
     {
-        for (int j = 0; j < M - 1; ++j)
+        for (int j = 0; j < M; ++j)
         {
             float cellBorder = 0;
-            m_cellsVA[_indexOfC(i, j)    ].position = sf::Vector2f{m_cellSize * i + cellBorder, m_cellSize * j + cellBorder};
-            m_cellsVA[_indexOfC(i, j) + 1].position = sf::Vector2f{m_cellSize * (i + 1) - cellBorder, m_cellSize * j + cellBorder};
-            m_cellsVA[_indexOfC(i, j) + 2].position = sf::Vector2f{m_cellSize * i + cellBorder, m_cellSize * (j + 1) - cellBorder};
-            m_cellsVA[_indexOfC(i, j) + 3].position = sf::Vector2f{m_cellSize * (i + 1) - cellBorder, m_cellSize * j + cellBorder};
-            m_cellsVA[_indexOfC(i, j) + 4].position = sf::Vector2f{m_cellSize * i + cellBorder, m_cellSize * (j + 1) - cellBorder};
-            m_cellsVA[_indexOfC(i, j) + 5].position = sf::Vector2f{m_cellSize * (i + 1) - cellBorder, m_cellSize * (j + 1) - cellBorder};
+            int idx = _indexOfC(i, j);
+            m_cellsVA[idx + 0].position = sf::Vector2f{m_cellSize * (i - .5f) + cellBorder, m_cellSize * (j - .5f) + cellBorder};
+            m_cellsVA[idx + 1].position = \
+            m_cellsVA[idx + 3].position = sf::Vector2f{m_cellSize * (i + .5f) - cellBorder, m_cellSize * (j - .5f) + cellBorder};
+            m_cellsVA[idx + 2].position = \
+            m_cellsVA[idx + 4].position = sf::Vector2f{m_cellSize * (i - .5f) + cellBorder, m_cellSize * (j + .5f) - cellBorder};
+            m_cellsVA[idx + 5].position = sf::Vector2f{m_cellSize * (i + .5f) - cellBorder, m_cellSize * (j + .5f) - cellBorder};
         }
     }
 }
@@ -27,10 +28,11 @@ void FluidDisplay::draw(sf::RenderTarget& target, sf::RenderStates states) const
         {
             if (const auto [v, free] = m_sim.edgeX(i, j); !free && v == 0)
             {
-                m_wallsVA[_indexOfX(i, j)].position = sf::Vector2f{m_cellSize * i, m_cellSize * (j)};
-                m_wallsVA[_indexOfX(i, j) + 1].position = sf::Vector2f{m_cellSize * i, m_cellSize * (j + 1)};
-                m_wallsVA[_indexOfX(i, j)].color = sf::Color::Black;
-                m_wallsVA[_indexOfX(i, j) + 1].color = sf::Color::Black;
+                int idx = _indexOfX(i, j);
+                m_wallsVA[idx + 0].position = sf::Vector2f{m_cellSize * i, m_cellSize * (j)};
+                m_wallsVA[idx + 1].position = sf::Vector2f{m_cellSize * i, m_cellSize * (j + 1)};
+                m_wallsVA[idx + 0].color = sf::Color::Black;
+                m_wallsVA[idx + 1].color = sf::Color::Black;
             }
             else
             {
@@ -47,10 +49,11 @@ void FluidDisplay::draw(sf::RenderTarget& target, sf::RenderStates states) const
         {
             if (const auto [v, free] = m_sim.edgeY(i, j); !free && v == 0)
             {
-                m_wallsVA[_indexOfY(i, j)].position = sf::Vector2f{m_cellSize * (i), m_cellSize * j};
-                m_wallsVA[_indexOfY(i, j) + 1].position = sf::Vector2f{m_cellSize * (i + 1), m_cellSize * j};
-                m_wallsVA[_indexOfY(i, j)].color = sf::Color::Black;
-                m_wallsVA[_indexOfY(i, j) + 1].color = sf::Color::Black;
+                int idx = _indexOfY(i, j);
+                m_wallsVA[idx + 0].position = sf::Vector2f{m_cellSize * (i), m_cellSize * j};
+                m_wallsVA[idx + 1].position = sf::Vector2f{m_cellSize * (i + 1), m_cellSize * j};
+                m_wallsVA[idx + 0].color = sf::Color::Black;
+                m_wallsVA[idx + 1].color = sf::Color::Black;
             }
             else
             {
@@ -62,16 +65,17 @@ void FluidDisplay::draw(sf::RenderTarget& target, sf::RenderStates states) const
         }
     }
 
-    for (int i = 0; i < N - 1; ++i)
+    for (int i = 0; i < N; ++i)
     {
-        for (int j = 0; j < M - 1; ++j)
+        for (int j = 0; j < M; ++j)
         {
-            m_cellsVA[_indexOfC(i, j)].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j)].position), 0.5));
-            m_cellsVA[_indexOfC(i, j) + 1].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j) + 1].position), 0.5));
-            m_cellsVA[_indexOfC(i, j) + 2].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j) + 2].position), 0.5));
-            m_cellsVA[_indexOfC(i, j) + 3].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j) + 3].position), 0.5));
-            m_cellsVA[_indexOfC(i, j) + 4].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j) + 4].position), 0.5));
-            m_cellsVA[_indexOfC(i, j) + 5].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[_indexOfC(i, j) + 5].position), 0.5));
+            int idx = _indexOfC(i, j);
+            m_cellsVA[idx + 0].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[idx + 0].position), 0.5));
+            m_cellsVA[idx + 1].color = \
+            m_cellsVA[idx + 3].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[idx + 3].position), 0.5));
+            m_cellsVA[idx + 2].color = \
+            m_cellsVA[idx + 4].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[idx + 4].position), 0.5));
+            m_cellsVA[idx + 5].color = lerp({64, 64, 64}, sf::Color::White, std::pow(m_sim.computeDensity(m_cellsVA[idx + 5].position), 0.5));
         }
     }
 
